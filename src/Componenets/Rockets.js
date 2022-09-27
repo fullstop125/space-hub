ae249f597a2aa31e8dbbab6f6d3d33e0513c8828
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-27 13:31:25 +0500

    Define Indiviusal Rocket as a seprate Componenet

--- src/Componenets/Rockets.js
+++ src/Componenets/Rockets.js
@@ -1,4 +1,18 @@
-const Rockets = () => (
-  <h1>Rockets Will Go Here</h1>
-);
+const Rockets = ({ rocketImage, rocketName, rocketDescription }) => {
+  Rockets.propTypes = {
+    rocketImage: PropTypes.string.isRequired,
+    rocketName: PropTypes.string.isRequired,
+    rocketDescription: PropTypes.string.isRequired,
+  };
+  return (
+    <article className="rocket-container">
+      <img src={rocketImage} alt={rocketName} />
+      <div className="rocket-details">
+        <h2>{rocketName}</h2>
+        <p>{rocketDescription}</p>
+        <button type="submit" className="btn btn-primary">Reserve</button>
+      </div>
+    </article>
+  );
+};
 export default Rockets;
