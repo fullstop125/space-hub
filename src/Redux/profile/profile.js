bcf9addd6315373b0b7b78140c3eaaad81d450d0
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-09-28 14:19:35 +0500

    Added Reducer and Action for Profile

---
+++ src/Redux/profile/profile.js
@@ -0,0 +1,34 @@
+const RESERVED = 'space-hub/profile/RESERVED';
+const JOINED = 'space-hub/profile/JOINED';
+
+export const reserved = (payload) => ({
+  type: RESERVED,
+  payload,
+});
+
+export const joined = (payload) => ({
+  type: JOINED,
+  payload,
+});
+
+const initialState = {
+  reserved: [],
+  joined: [],
+};
+
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    case RESERVED:
+      return {
+        ...state,
+        reserved: action.payload,
+      };
+    case JOINED:
+      return {
+        ...state,
+        joined: action.payload,
+      };
+    default:
+      return state;
+  }
+}
